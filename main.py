31229ecb283b43cafd4ac2744aa22408f390e264
chrisfoulon <hd.chrisfoulon@gmail.com>
2017-04-06 08:03:02 +0000

    main and main_frame and gitignore added

---
+++ main.py
@@ -0,0 +1,15 @@
+# -*-coding:Utf-8 -*
+
+from PyQt5.QtWidgets import (QStyleFactory, QApplication)
+
+if __name__ == '__main__':
+    import sys
+    import view.main_frame as mf
+
+    app = QApplication(sys.argv)
+    app.setStyle(QStyleFactory.create("fusion"))
+
+    main_frame = mf.mainFrame()
+    main_frame.show()
+
+    sys.exit(app.exec_())
---
+++ view/main_frame.py
@@ -0,0 +1,27 @@
+# -*-coding:Utf-8 -*
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QWidget, QLCDNumber, QSlider,
+    QVBoxLayout, QApplication, QDesktopWidget)
+
+class mainFrame(QWidget):
+    def __init__(self, parent=None):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        self.resize(250, 150)
+        self.center()
+
+        self.setWindowTitle('Center')
+        self.show()
+
+
+    def center(self):
+
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
