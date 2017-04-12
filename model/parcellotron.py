c5fcebc8f78ef30f67c436facfcff10d87269888
chrisfoulon <hd.chrisfoulon@gmail.com>
2017-04-12 14:38:13 +0000

    function to find substring in a file path in utils.py and begining of the code for checking inputs in tracto_4D objects

--- model/parcellotron.py
+++ model/parcellotron.py
@@ -5,9 +5,9 @@ import nibabel as nib
 import numpy as np
 import utils as ut
 import os
+import textwrap
 
-test_path = "/root/user/filename.ext1.ext2"
-os.path.basename(ut.split_ext(test_path)[0])
+software_name = "COBRA"
 
 class parcellotron(metaclass=abc.ABCMeta):
     """ Abstract class of an generic object which will contain informations
@@ -17,9 +17,14 @@ class parcellotron(metaclass=abc.ABCMeta):
     def __init__(self, subj_path, modality):
         self.modality = modality
         self.subj_folder = subj_path
-        self.subj_name = os.path.basename(subj_name)
+        self.subj_name = os.path.basename(subj_path)
         self.root_dir = os.path.dirname(subj_path)
         self.input_dir = os.path.join(subj_path, modality)
+        print(self.input_dir)
+        self.res_dir = os.path.join(self.input_dir, software_name + "_results")
+        if not os.path.exists(self.res_dir):
+            os.mkdir(self.res_dir)
+        assert self.verify_input_folder(self.input_dir), self.inputs_needed()
 
     @abc.abstractmethod
     def read_inputs_into_2D(self):
@@ -29,6 +34,16 @@ class parcellotron(metaclass=abc.ABCMeta):
     def map_ROIs(self):
         pass
 
+    @abc.abstractmethod
+    def verify_input_folder(self, path):
+        pass
+
+    @abc.abstractmethod
+    def inputs_needed(self):
+        """ Display a message to explain what input files you need and which
+        name you need to indentify those files.
+        """
+        pass
 
 
 class tracto_4D(parcellotron):
@@ -37,11 +52,14 @@ class tracto_4D(parcellotron):
     Parameters
     ----------
     subj_path : str
-    The path to the folder containing the different modality folders which
-    contain the inputs.
+        The path to the folder containing the different modality folders which
+        contain the inputs.
     """
-    def __init__(self, subj_path):
+    def __init__(self, subj_path, hemi='ALL'):
         super().__init__(subj_path, self.__class__.__name__)
+        assert hemi == 'LH' or hemi == 'RH' or hemi == 'ALL', "The hemi \
+        parameter can be LH RH or ALL"
+        self.hemi = hemi
         print(self.subj_name)
 
     def read_inputs_into_2D(self, subj_path, hemi):
@@ -50,28 +68,71 @@ class tracto_4D(parcellotron):
         Parameters
         ----------
         subj_path : str
-        The path to the subject folder, containing the 4D image.
-        (The filename must contain "4Dcmaps" separated by underscores)
+            The path to the subject folder, containing the 4D image.
+            (The filename must contain "4Dcmaps" separated by underscores)
         hemi : {'LH', 'RH', 'ALL'}, optional (default 'ALL')
-        Choose the hemisphere you want to parcellate (LH for left hemisphere;
-        RH for the right one and ALL for both). The function will look for the
-        file containing 'LH', 'RH' or none of those (for ALL)
+            Choose the hemisphere you want to parcellate (LH for left
+            hemisphere; RH for the right one and ALL for both). The function
+            will look for the file containing 'LH', 'RH' or none of those
+            (for 'ALL')
         Returns
         -------
+        2D_con_mat : 2D np.array
+            2D matrix where rows are seed ROIs and columns the target's voxels
 
+        Notes
+        -----
+        2D_con_mat is also stored in "temprorary_files" in the results folder
+        with the name : subj[_hemi]_2D_connectivity_matrix.npy
         """
         print("Olol")
 
     def map_ROIs(self):
         print("ok")
 
+    def verify_input_folder(self, path, hemi='ALL'):
+        self.cmaps
 
-test = tracto_4D("test", "test_dir")
+        if hemi == 'ALL':
+
+        else:
+
+        return(False)
+
+    def inputs_needed(self):
+        """
+        Returns
+        -------
+        message : str
+            A string describing the inputs you need for this modality
+        """
+        message = textwrap.dedent("""\
+            Inputs needed for this modality :
+            1) subj[_hemi]_4Dcmaps.nii[.gz] a 4D image with a connectivity map
+                for each time point
+            2) subj[_hemi]_ribbon.nii[.gz] the 3D binary mask of the brain ribbon
+            3) subj[_hemi]_ROIs.nii[.gz] 3D file with values indexing
+                Regions of Interest (ROIs).
+            [_hemi] can be LH or RH if you want to calculate the parcellation
+            for the left or the right hemisphere respectively""")
+        return message
+
+# %%
+test = tracto_4D("/data/BCBLab/test_COBRA/S1").inputs_needed()
 test.map_ROIs()
+# %%
 import os
 st = os.path.join("blabla", "bliblibli")
+
+# os.rmdir("blibli")
+print(st)
+tt = """test
+test bla"""
 type(st)
 def func(str):
     assert len(str) > 2, "Error lol"
     print(str)
-func("eéé")
+func("1")
+def returns_str():
+    return "une string"
+assert 2 == 1, returns_str()
--- model/utils.py
+++ model/utils.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from os.path import splitext
+from os.path import splitext, basename
 
 def split_ext(path):
     """ Split the file extension from the path even in case of double extension.
@@ -15,3 +15,24 @@ def split_ext(path):
     if len(path.split('.')) > 2:
         return path.split('.')[0],'.'.join(path.split('.')[-2:])
     return splitext(path)
+
+def find_in_filename(path, string):
+    """ Find string in the filename of path (only in the basename).
+        Note that the filename is splitted by underscores.
+    Parameters
+    ----------
+    path : str
+        the path of the filename you want to parse
+    string : str
+        the string you want to find in path
+    Returns
+    -------
+    is_in : bool
+        True if the filename contains string
+        False if not
+    """
+    base = basename(path)
+    filename = split_ext(base)[0]
+    splitted = filename.split('_')
+    is_in = string in splitted
+    return is_in
