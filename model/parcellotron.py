e4964854357ca2c520f1960a6725261adc6cc5d6
chrisfoulon <hd.chrisfoulon@gmail.com>
2017-04-13 12:28:48 +0000

    autodafe of the hemi propoerty/parameter/prefix because it was a mess and we don't really need it.

--- model/parcellotron.py
+++ model/parcellotron.py
@@ -13,6 +13,23 @@ software_name = "COBRA"
 class parcellotron(metaclass=abc.ABCMeta):
     """ Abstract class of an generic object which will contain informations
     used to parcellate an image.
+    Attributes
+    ----------
+    modality : {'tracto_4D', 'tracto_matrix', 'fmri_4D'}
+        Name of the modality
+    subj_folder : str
+        Path to the subject folder
+    subj_name : str
+        Name of the subject
+    root_dir : str
+        Parent directory of the subject folder (used especially for group level
+        analysis)
+    input_dir : str
+        Directory containing the inputs of the subject for this modality
+    res_dir : str
+        Folder which will contain the different results of the software, for
+        this modality. The folder is created when the object is instanciate if
+        it does not exist.
     """
     @abc.abstractmethod
     def __init__(self, subj_path, modality):
@@ -21,7 +38,6 @@ class parcellotron(metaclass=abc.ABCMeta):
         self.subj_name = os.path.basename(subj_path)
         self.root_dir = os.path.dirname(subj_path)
         self.input_dir = os.path.join(subj_path, modality)
-        print(self.input_dir)
         self.res_dir = os.path.join(self.input_dir, software_name + "_results")
         if not os.path.exists(self.res_dir):
             os.mkdir(self.res_dir)
@@ -55,15 +71,29 @@ class tracto_4D(parcellotron):
     subj_path : str
         The path to the folder containing the different modality folders which
         contain the inputs.
+    Attributes
+    ----------
+    INHERITED
+    modality : {'tracto_4D', 'tracto_matrix', 'fmri_4D'}
+        Name of the modality
+    subj_folder : str
+        Path to the subject folder
+    subj_name : str
+        Name of the subject
+    root_dir : str
+        Parent directory of the subject folder (used especially for group level
+        analysis)
+    input_dir : str
+        Directory containing the inputs of the subject for this modality
+    res_dir : str
+        Folder which will contain the different results of the software, for
+        this modality. The folder is created when the object is instanciate if
+        it does not exist.
     """
-    def __init__(self, subj_path, hemi='ALL'):
+    def __init__(self, subj_path):
         super().__init__(subj_path, self.__class__.__name__)
-        assert hemi == 'LH' or hemi == 'RH' or hemi == 'ALL', "The hemi \
-        parameter can be LH RH or ALL"
-        self.hemi = hemi
-        print(self.subj_name)
 
-    def read_inputs_into_2D(self, subj_path, hemi):
+    def read_inputs_into_2D(self, subj_path):
         """ Read the inputs and tranform the 4D image into a 2D connectivity
         matrix.
         Parameters
@@ -71,11 +101,6 @@ class tracto_4D(parcellotron):
         subj_path : str
             The path to the subject folder, containing the 4D image.
             (The filename must contain "4Dcmaps" separated by underscores)
-        hemi : {'LH', 'RH', 'ALL'}, optional (default 'ALL')
-            Choose the hemisphere you want to parcellate (LH for left
-            hemisphere; RH for the right one and ALL for both). The function
-            will look for the file containing 'LH', 'RH' or none of those
-            (for 'ALL')
         Returns
         -------
         2D_con_mat : 2D np.array
@@ -84,21 +109,17 @@ class tracto_4D(parcellotron):
         Notes
         -----
         2D_con_mat is also stored in "temprorary_files" in the results folder
-        with the name : subj[_hemi]_2D_connectivity_matrix.npy
+        with the name : subj_2D_connectivity_matrix.npy
         """
         print("Olol")
 
     def map_ROIs(self):
         print("ok")
 
-    def verify_input_folder(self, in_path, hemi='ALL'):
+    def verify_input_folder(self, in_path):
         boo = False
 
-        if hemi == 'ALL':
-
-        else:
-
-        return(False)
+        return boo
 
     def inputs_needed(self):
         """
@@ -109,13 +130,12 @@ class tracto_4D(parcellotron):
         """
         message = textwrap.dedent("""\
             Inputs needed for this modality :
-            1) subj[_hemi]_4Dcmaps.nii[.gz] a 4D image with a connectivity map
+            1) subj_4Dcmaps.nii[.gz] a 4D image with a connectivity map
                 for each time point
-            2) subj[_hemi]_ribbon.nii[.gz] the 3D binary mask of the brain ribbon
-            3) subj[_hemi]_ROIs.nii[.gz] 3D file with values indexing
+            2) subj_ribbon.nii[.gz] the 3D binary mask of the brain ribbon
+            3) subj_ROIs.nii[.gz] 3D file with values indexing
                 Regions of Interest (ROIs).
-            [_hemi] can be LH or RH if you want to calculate the parcellation
-            for the left or the right hemisphere respectively""")
+            """)
         return message
 
 # %%
@@ -137,9 +157,3 @@ func("1")
 def returns_str():
     return "une string"
 assert 2 == 1, returns_str()
-import re
-st1 = "subj_LH_cmaps4D.nii.gz"
-st2 = "subj_cmaps4D_LH.nii.gz"
-st3 = "subj_cmaps4D_RH.nii.gz"
-str_arr = ["LH", 'cmaps4D']
-ut.find_in_filename(st3, str_arr)
--- model/utils.py
+++ model/utils.py
@@ -16,27 +16,23 @@ def split_ext(path):
         return path.split('.')[0],'.'.join(path.split('.')[-2:])
     return splitext(path)
 
-def find_in_filename(path, str_arr):
+def find_in_filename(path, string):
     """ Find string in the filename of path (only in the basename).
         Note that the filename is splitted by underscores.
     Parameters
     ----------
     path : str
         the path of the filename you want to parse
-    str_arr : str[]
-        the strings you want to find in path
+    string : str
+        the string you want to find in path
     Returns
     -------
     is_in : bool
         True if the filename contains string
         False if not
     """
-    assert len(str_arr) > 0, "The string array is empty"
-    assert not "" in str_arr, "str_arr contains an empty string"
     base = basename(path)
     filename = split_ext(base)[0]
     splitted = filename.split('_')
-    is_in = True
-    for s in str_arr:
-        is_in = is_in and (s in splitted)
+    is_in = string in splitted
     return is_in
